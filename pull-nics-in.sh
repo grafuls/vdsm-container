a90b59191b9b6995910775a6dd9548d5090bca14
Tolik Litovsky <tlitovsk@redhat.com>
2016-03-20 18:36:28 +0200

    Fixing the pull in nics script
    Adding temp dir creation.
    Use the variables corectly.
    Removing local where not needed
    
    Signed-off-by: Tolik Litovsky <tlitovsk@redhat.com>

--- pull-nics-in.sh
+++ pull-nics-in.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -xe
 
-local tmp=$(mktemp -d -u --tmpdir=/host/tmp)
-local container_tmp=${tmp#/host}
-local container_id=$(chroot /host docker ps -q -f "ancestor=tlitovsk/vdsm-container" -f "status=running")
+tmp=$(mktemp -d -u --tmpdir=/host/tmp)
+mkdir -p $tmp
+container_tmp=${tmp#/host}
+container_id=$(chroot /host docker ps -q -f "ancestor=tlitovsk/vdsm-container" -f "status=running")
 
-cp /root/move-nics.sh $tmp
-chroot /container_tmp/move-nics.sh $container_id
+cp /root/move-nics.sh $tmp/move-nics.sh
+chroot /host ./$container_tmp/move-nics.sh $container_id
 
 rm -rf $tmp
